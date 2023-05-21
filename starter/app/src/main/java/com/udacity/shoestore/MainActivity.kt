d97a4cc230976b3c659303f53351bc8ecb8583a1
cindy <cindyshen001@gmail.com>
2023-05-21 12:10:43 -0700

    navigate to the Welcome Screen

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,17 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import timber.log.Timber
+import com.udacity.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
 }
+
+
--- starter/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -1,10 +1,14 @@
 package com.udacity.shoestore.login
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +21,18 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.btnCreateAccount.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        return binding.root
     }
 }
